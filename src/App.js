b7e11c9cb6ab6a9a7de8c7922ad0e01ab02bcdca
mrwaiyanphyoe <waiyanphyoee@yahoo.com>
2022-07-15 00:33:55 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Test from "./test";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Test />
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import "./test.css";
+
+function Test() {
+	return (
+		<div className="Test">
+			<h1>Hi, I'm Wai Yan Phyoe</h1>
+		</div>
+	);
+}
+
+export default Test;
